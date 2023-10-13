8e3946e929b85456aa1771f5594e58a984bf81d6
Vaishvi Gupta <vaishvigupta2708@gmail.com>
2023-10-13 22:48:04 +0530

    first

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const api = {  
+  tagsEndPoint: `${process.env.REACT}/tags/`,
+  usersEndPoint: `${process.env.REACT}/users/`,
+  postsEndPoint: `${process.env.REACT}/posts/`,
+  repliesEndPoint: `${process.env.REACT}/reply/`,
+}
